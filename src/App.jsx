bc211e061bcf730739adc0d60a6421e9e204ec05
Niven Asmitha Liyanage <nivenasmitha345@gmail.com>
2025-07-01 15:32:17 -0700

    feat: add Banner component to App for enhanced layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
 import TopProducts from "./components/TopProducts/TopProducts";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Products />
       <TopProducts />
+      <Banner />
     </div>
   );
 }
